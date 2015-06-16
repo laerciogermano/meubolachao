4823a4a00325251f5fdecec39d10100051005003
laerciogermano <laerciogermanoo@gmail.com>
2015-06-15 22:10:45 -0300

    metodo create

--- models/Artista.js
+++ models/Artista.js
@@ -10,6 +10,35 @@ module.exports = function(){
 		foto : String
 	});
 
+	function create(obj, callback){
+		if(!obj)
+			return callback({msg : 'Objeto vazio'});
+
+		if(!obj.nome)
+			return callback({msg : 'Nome vazio'});
+
+		if(!obj.foto)
+			return callback({msg : 'Foto vazia'});
+
+		var Artista = mongoose.model('Artista');
+		Artista.findOne({nome : obj.nome}, function(err, data){
+			if(err)
+				return callback({msg : 'Erro ao encontrar artista'});
+			
+			if(data)
+				return callback({msg : 'Já existe artista com esse nome'});
+
+			var newArtista = new Artista(obj);
+			newArtista.save(function(err, _data){
+				if(err)
+					return callback({msg : 'Erro ao salvar artista'});
+
+				callback(false, _data);
+			});
+		});
+	};
+
+	ArtistaSchema.methods.create = create;
 	mongoose.model('Artista', ArtistaSchema);
 };
 
