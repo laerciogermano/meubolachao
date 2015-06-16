b718071d8c4768d39cb0590f1d2287f0a0420761
root <otavio@otavio.com>
2015-06-15 22:30:45 -0300

    Create do front

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,20 @@
 var app = angular.module('app', []);
 
+
 app.controller('controller', ['$scope', '$http', function($scope, $http){
+	$scope.artista = [];
+	$scope.criarArtista = {};
+
+	$scope.Create = function(){
+		$http.post('/api/artista/create', $scope.criar)
+		.success(function(data){
+			console.log(data);
+		})
+		.error(function(data){
+			console.log(data);	
+		})	
+
+	}
 	
 }]);
 
